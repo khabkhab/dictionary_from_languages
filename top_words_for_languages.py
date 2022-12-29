7537249afa4360e748cd5e978bafd1489aef6627
khabkhab <khabibyusupov2014@gmail.com>
2022-12-30 00:23:05 +0100

    Simplified version. Dictionary works. PDF not working

--- top_words_for_languages.py
+++ top_words_for_languages.py
@@ -1,46 +1,38 @@
 from googletrans import Translator
 import pandas as pd
+import string
+import fpdf
+
 
 # opens the text file
 
 def read_file(name):
     with open(name, 'r', encoding='utf8') as file:
-        text = file.read()
-        return text
+        for line in file:
+            line = exclude_characters(line)
+            words = line.split(" ")
+            for word in words:
+                word = word.lower()
+                if word not in dictionary:
+                    dictionary[word] = 0
+                dictionary[word] += 1
 
-# split text file
-
-def devided_text(text):
-    return text.split()
 
 # exceptions from the text
-def exclude_exceptions():
-    print 
-    
-# counts the words
+def exclude_characters(line):
+    for character in string.punctuation:
+        line = line.replace(character, "")
+    return line
 
-dictionary = {}
-def create_dic(text):
-    global dictionary
-    for item in devided_text(text):
-        dictionary.update({item:text.count(item)})
 # sort the dictionary
-
-def sort_dic(dictionary):
-    sorted_dic = sorted(dictionary.items(), key = lambda item: item[1], reverse = True)
-    return sorted_dic
-
-
 
 # identifies how many words to return
 
 def number_of_words():
     restrictred_list = []
-    for item in sort_dic(dictionary):
-        if len(item[0]) > 3 and item[0].isalnum():    
-            restrictred_list.append(item[0].lower())
-    restrictred_list_2 = restrictred_list[:length]
-    return restrictred_list_2
+    for item in dictionary:
+        restrictred_list.append(item) 
+    return restrictred_list[:length]
 
 
 # add translations with respect to language
@@ -52,16 +44,22 @@ def translate_the_top(w, language):
 
 # create dataframe from two lists: top_lenght and translated version
 def create_the_dataframe():
-    df = pd.DataFrame(list(zip(number_of_words(), [translate_the_top(the_word, language) for the_word in number_of_words()])))
+    df = pd.DataFrame(list(zip(number_of_words(), [translate_the_top(the_word, language).text for the_word in number_of_words()])), columns=['Original Top', 'Translated Top'])
     return df
 
-# save as pdf file
-
+# save df as pdf file without first column (numbered rows) !!!!not finished
+def create_pdf(dataframe):
+    pdf = FPDF()
+    pdf.add_page()
+    pdf.set_font('Times', '', 12)
+    pdf.cell()
+    pdf.output(f"List of top {length} words in the {name_of_file} with tranlsation to {language}", "F")
+    
 #
-
-symbols = [',', '.', '`', '“']
+dictionary = {}
 name_of_file = input("Please write the name of the file: ")
 length = int(input("How many words do you want to extract from the text?: "))
 language = input("Which language do you want to translate the top words to?: ").lower()
-create_dic(read_file(name_of_file))
-print(number_of_words())
+
+read_file(name_of_file)
+create_pdf(create_the_dataframe())
