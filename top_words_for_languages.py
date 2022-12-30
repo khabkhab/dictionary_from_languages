9b2c80cb8a8d32d28db4653b016fba4fae9120cf
khabkhab <khabibyusupov2014@gmail.com>
2022-12-30 16:59:50 +0100

    Changed pdf to csv. Added recognizing language (language_keys) function

--- top_words_for_languages.py
+++ top_words_for_languages.py
@@ -1,7 +1,8 @@
 from googletrans import Translator
+import googletrans
 import pandas as pd
 import string
-import fpdf
+from pathlib import Path 
 
 
 # opens the text file
@@ -12,10 +13,11 @@ def read_file(name):
             line = exclude_characters(line)
             words = line.split(" ")
             for word in words:
-                word = word.lower()
-                if word not in dictionary:
+                word = word.capitalize()
+                if word not in dictionary and word != "\n": 
                     dictionary[word] = 0
-                dictionary[word] += 1
+                elif word != "\n":
+                    dictionary[word] += 1
 
 
 # exceptions from the text
@@ -24,8 +26,11 @@ def exclude_characters(line):
         line = line.replace(character, "")
     return line
 
-# sort the dictionary
-
+# converts language value to language key, i.e., english to en, spanish to es, etc
+def language_keys(lang):
+    for key, val in googletrans.LANGUAGES.items():
+        if val == lang.lower():
+            return key
 # identifies how many words to return
 
 def number_of_words():
@@ -38,28 +43,26 @@ def number_of_words():
 # add translations with respect to language
 def translate_the_top(w, language):
     translate = Translator()
-    translated_word = translate.translate(w, dest=language)
+    translated_word = translate.translate(w, dest=language_keys(language))
     return translated_word
 
 
 # create dataframe from two lists: top_lenght and translated version
 def create_the_dataframe():
     df = pd.DataFrame(list(zip(number_of_words(), [translate_the_top(the_word, language).text for the_word in number_of_words()])), columns=['Original Top', 'Translated Top'])
+    # res = {number_of_words()[0]: [translate_the_top(the_word, language).text for the_word in number_of_words()][i] for i in range(len(number_of_words()))}
     return df
 
-# save df as pdf file without first column (numbered rows) !!!!not finished
-def create_pdf(dataframe):
-    pdf = FPDF()
-    pdf.add_page()
-    pdf.set_font('Times', '', 12)
-    pdf.cell()
-    pdf.output(f"List of top {length} words in the {name_of_file} with tranlsation to {language}", "F")
-    
-#
+# save df as csv file without first column (numbered rows) !!!!not finished
+
+def data_to_csv(df):
+    filepath = Path(f'Top {length} words with translation to {language}.csv')
+    df.to_csv(filepath)
+# detect language function
 dictionary = {}
 name_of_file = input("Please write the name of the file: ")
 length = int(input("How many words do you want to extract from the text?: "))
 language = input("Which language do you want to translate the top words to?: ").lower()
-
 read_file(name_of_file)
-create_pdf(create_the_dataframe())
+# print(create_the_dataframe()[:15])
+data_to_csv(create_the_dataframe())
