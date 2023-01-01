b089a0cee4704d6d2c3ff98bf6297985da56c7f8
khabkhab <khabibyusupov2014@gmail.com>
2023-01-01 18:18:44 +0100

    Added function of names and country; exclude top names based on country

--- top_words_for_languages.py
+++ top_words_for_languages.py
@@ -3,6 +3,8 @@ import googletrans
 import pandas as pd
 import string
 from pathlib import Path 
+from names_dataset import NameDataset
+import pycountry
 
 
 # opens the text file
@@ -20,9 +22,11 @@ def read_file(name):
                 words = list(filter(not_an_article, line.split(" "))) #filter out all the articles
                 for word in words:
                     word = word.capitalize()
-                    if word not in dictionary and word != "\n": 
+                    if word not in dictionary:
+                        word.split() 
                         dictionary[word] = 0
-                    elif word != "\n":
+                    else:
+                        word.split()
                         dictionary[word] += 1    
 
 # excludes special characters from text
@@ -33,8 +37,10 @@ def exclude_characters(line):
 
 # filter that excludes articles from frequent words
 def not_an_article(word):
-    article_list = ["the", "a", "an", " ", "—", "\"", "\n"]
-    if word.lower() in article_list or word.lower() in [str(x) for x in range(10)]:
+    article_list = ["the", "a", "an", " ", "—", "\"", "\n", "\'", "\t", "\r"]
+    merged = article_list + numbers
+    #  "potter", "dursley", "harry", "potter", "rowling"
+    if word.lower() in merged or word.capitalize() in list_of_names:
         return False
     return True
 
@@ -57,6 +63,19 @@ def translate_the_top(w, language):
     translated_word = translate.translate(w, dest=language_keys(language))
     return translated_word
 
+# turns the name of country into its alpha_2 version
+def get_country(country):
+    shorter = pycountry.countries.get(name=country).alpha_2
+    return shorter
+
+# creates a list of most common names based on the country
+def names(shorter):
+    names = NameDataset().get_top_names(100, use_first_names=True, country_alpha2=shorter)
+    male_list = [name for name in names[shorter]["M"]]
+    female_list = ([name for name in names[shorter]["F"]]) 
+    name_list = male_list + female_list
+    return name_list
+
 # create dataframe from two lists: top_lenght and translated version
 def create_the_dataframe():
     df = pd.DataFrame(list(zip(number_of_words(), [translate_the_top(the_word, language).text.capitalize() for the_word in number_of_words()])), columns=['Original Top', 'Translated Top'])
@@ -65,11 +84,12 @@ def create_the_dataframe():
 # save df as csv file without first column (numbered rows) !!!!not finished
 def data_to_csv(df):
     # filepath = Path(f'Top {length} words with translation to {language}.csv')
-    df.to_csv(Path(f'Top {length} words with translation to {language}.csv'), index=False)
-   
+    df.to_csv(Path(f'Top {length} words with translation to {language.capitalize()}.csv'), index=False)
+
 #introducing main function
 def main():
     read_file(name_of_file)
+    # print(create_the_dataframe()[115:125])
     data_to_csv(create_the_dataframe())
     
 #making the file usable by others with name==main
@@ -78,8 +98,10 @@ if __name__ == "__main__":
     name_of_file = input("Please write the name of the file: ")
     length = int(input("How many words do you want to extract from the text?: "))
     language = input("Which language do you want to translate the top words to?: ").lower()
+    list_of_names = names(get_country(input("Write down the name of country to exclude most common names: ")))
+    numbers = [str(num) for num in range(10)]
     if language_keys(language):
         main()
     else:
         print("Could not identify language, try again.")
-    
+        
